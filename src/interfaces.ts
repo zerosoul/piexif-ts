63eb1cb1802a69f8f8f9698c1442e04c6b5cff24
Joachim Holwech <joachim.holwech@gmail.com>
2019-05-10 14:21:00 +0100

    2.0.10

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,9 +1,8 @@
 export interface IExif {
-    '0th'?: any;
-    'Exif'?: any;
-    'Interop'?: any;
-    'GPS'?: any;
-    '1st'?: any;
-    'thumbnail'?: string;
+  '0th'?: any;
+  Exif?: any;
+  Interop?: any;
+  GPS?: any;
+  '1st'?: any;
+  thumbnail?: string;
 }
-
