888afe7a71ab2de92387dae271851ea069e87d77
Tristan Yang <yanggc888@163.com>
2024-09-07 11:18:35 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,27 @@ import { eslint } from 'rollup-plugin-eslint';
 
 export default {
   input: './src/index.ts',
-  output: {
-    file: './dist/piexif.js',
-    name: 'piexif',
-    format: 'umd',
-    sourcemap: true,
-  },
+  output: [
+    {
+      file: './dist/piexif.js',
+      name: 'piexif',
+      format: 'umd',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/piexif.es.js',
+      name: 'piexif',
+      format: 'es',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/piexif.browser.js',
+      name: 'piexif',
+      format: 'iife',
+      name: 'piexif',
+      sourcemap: true,
+    }
+  ],
   plugins: [
     typescript(),
     eslint({ include: 'src/', throwOnError: true, throwOnWarning: true }),
